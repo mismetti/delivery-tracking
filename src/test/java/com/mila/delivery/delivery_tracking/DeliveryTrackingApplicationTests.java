92039168dd94ea62792a6f9f0de0adea21d87c91
mismetti <milastrokes@gmail.com>
2026-01-13 19:04:07 -0300

    first commit

---
+++ src/test/java/com/mila/delivery/delivery_tracking/DeliveryTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mila.delivery.delivery_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
